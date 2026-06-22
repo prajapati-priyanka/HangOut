30a57399a08ee61c1e574917b9f094f231f80f66
prajapati-priyanka <contact.priyankaprajapati.com>
2026-06-22 14:23:10 +0530

    Feat: Add Feed API

--- src/app.js
+++ src/app.js
@@ -3,12 +3,11 @@ const connectDB = require("./config/database.js");
 const { User } = require("./models/user.js");
 const app = express();
 
-app.use(express.json());
+app.use(express.json());  // use to parse the client request for all the routes.
 
+// signup API
 app.post("/signup", async (req, res) => {
   const newUser = req?.body;
-
-  // console.log("req", req?.body);
 
   try {
     const user = new User(newUser);
@@ -19,6 +18,21 @@ app.post("/signup", async (req, res) => {
   } catch (error) {
     res.status(404).send("Something went wrong" + error.message);
   }
+});
+
+// Feed API => Get All Users
+app.get("/user", async(req, res)=>{
+
+  try {
+   const users = await User.find({});
+  // console.log("users", users);
+  res.status(200).send(users);
+  } catch (error) {
+    res.status(404).send("Something went wrong" + error.message);
+    
+  }
+
+
 });
 
 // Database connection
